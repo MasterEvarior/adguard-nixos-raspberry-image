3ef287c04d1eeee0b748ded712ae3b98851040fe
Giannin <contact@giannin.ch>
2025-10-26 21:22:28 +0100

    chore: do stuff

--- README.md
+++ README.md
@@ -1,8 +1,16 @@
-# AdGuard Home NixOS Raspberry Pi Image(s)
+# AdGuard Home NixOS Raspberry Pi Image
 
+
+## Configuration
 
 ## Links, References & more
 - https://github.com/zimbatm/vm-image-nix-talk
 - https://crystalwobsite.gay/posts/2025-01-26-nixos_vm_image#writing-a-basic-configuration.nix-for-our-vm-image
-- https://github.com/nix-community/nixos-generators
 - https://nix.dev/tutorials/nixos/nixos-configuration-on-vm.html#
+
+### Generators
+- https://github.com/nix-community/nixos-generators
+
+### Testing
+- https://blog.thalheim.io/2023/01/08/how-to-use-nixos-testing-framework-with-flakes/
+- https://msfjarvis.dev/posts/writing-your-own-nix-flake-checks/
--- flake.nix
+++ flake.nix
@@ -43,5 +43,45 @@
           format = "vm";
         };
       };
+
+      checks.x86_64-linux = nixpkgs.legacyPackages.x86_64-linux.pkgs.testers.runNixOSTest {
+        name = "adguard";
+        nodes = {
+
+          server =
+            { pkgs, ... }:
+            {
+              imports = [
+                ../image/configuration.nix
+              ];
+
+              virtualisation.graphics = false;
+              environment.systemPackages = [ pkgs.curlMinimal ];
+            };
+
+          client =
+            { pkgs, ... }:
+            {
+              environment.systemPackages = [ pkgs.curlMinimal ];
+              virtualisation.graphics = false;
+            };
+
+        };
+
+        testScript = ''
+          start_all()
+
+          server.wait_for_unit("adguardhome.service")
+          server.wait_for_open_port(53)
+          server.wait_for_open_port(80)
+
+          with subtest("AdGuard Home is reachable from server"):
+            server.succeed("curl --fail http://127.0.0.1:80 | grep -q '<title>AdGuard Home</title>'")
+
+          with subtest("AdGuard Home is reachable from client"):
+            server.succeed("curl --fail http://server:80 | grep -q '<title>AdGuard Home</title>'")
+        '';
+
+      };
     };
 }
