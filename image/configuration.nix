1557a54da766686570e98c5979acad184bd7edc7
Giannin <contact@giannin.ch>
2026-01-08 20:07:15 +0100

    feat: add possibility to remove password from the user

--- image/configuration.nix
+++ image/configuration.nix
@@ -3,6 +3,9 @@
   vmConfig,
   ...
 }:
+let
+  initialPassword = if vmConfig.user.no_password then null else vmConfig.user.initial_password;
+in
 {
   imports = [
     ./services
@@ -19,9 +22,9 @@
   ];
 
   users.users.${vmConfig.user.username} = {
+    inherit initialPassword;
     isNormalUser = true;
     extraGroups = [ "wheel" ];
-    initialPassword = vmConfig.user.initial_password;
     openssh.authorizedKeys.keys = [ vmConfig.user.ssh_key ];
   };
 
