10ab213f4e0e3aabb7ec952609d8ba5db1856a98
Giannin <contact@giannin.ch>
2026-02-21 12:30:13 +0100

    test: add test for blocked services

--- tests/adguard/script.py
+++ tests/adguard/script.py
@@ -12,7 +12,9 @@ with subtest("AdGuard Home is reachable from server"):
     )
 
 with subtest("AdGuard Home has loaded specified lists via API"):
-    response = server.succeed("curl -s http://127.0.0.1:80/control/filtering/status")
+    response = server.succeed(
+        "curl -s http://127.0.0.1:80/control/filtering/status"
+    ).strip()
     data = json.loads(response)
 
     filter_names = [f["name"] for f in data.get("filters", [])]
@@ -23,6 +25,24 @@ with subtest("AdGuard Home has loaded specified lists via API"):
             server.log(f"Verified filter: {name}")
         else:
             server.fail(f"Filter '{name}' not found in AdGuard. Found: {filter_names}")
+
+with subtest("AdGuard Home has blocked specified services"):
+    response = server.succeed(
+        "curl -s http://127.0.0.1:80/control/blocked_services/get"
+    ).strip()
+
+    data = json.loads(response)
+
+    service_names = data.get("ids", [])
+
+    expected_services = ["facebook", "betano", "betfair", "blaze", "betway", "shein"]
+    for name in expected_services:
+        if name in service_names:
+            server.log(f"Verified blocked service: {name}")
+        else:
+            server.fail(
+                f"Service '{name}' not found in AdGuard. Found: {service_names}"
+            )
 
 with subtest("AdGuard Home is reachable from client"):
     client.succeed(
